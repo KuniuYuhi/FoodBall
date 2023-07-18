007ee5fa8ca2a7cf95a5535cb0a5e7b416c1784e
azuki <yuhi55.k@gmail.com>
2023-07-18 12:31:23 +0900

    アクタークラス追加。食べた食べ物の数変数追加

---
+++ Assets/Script/Actor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField,Header("H‚×•¨‚ğH‚×‚½”")]
+    int eatFoods = 0;
+
+    //Vector3 size
+
+    int GetEatFoods()
+    {
+        return eatFoods;
+    }
+   
+}
